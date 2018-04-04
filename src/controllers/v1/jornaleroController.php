27a9b5d91425640e82f4c723e91b7d0a5d50e4bf
anderssonflorez <florezanderson@thotstrategy.com>
2018-04-03 19:59:40 -0500

    archivo faltante por guardar del commit anterior

--- src/controllers/v1/jornaleroController.php
+++ src/controllers/v1/jornaleroController.php
@@ -2,10 +2,29 @@
 
 namespace \Src\Controller\v1;
 
-use Slim\Http\Request, Slim\Http\Request, Src\Libs\UtilResponse;
+use Slim\Http\Request, Slim\Http\Request, Src\Libs\UtilResponse, Exception;
 
 class JornaleroController{
+  private $app = null;
+  private $jornaleroModel = null;
+  private $utilResponse = null;
 
+  static $instance;
+
+  public function __construct($app){
+    $this->app = $app->getContainer();
+
+    $app->post('/jornalero', [$this,'createJornalero']);
+    $app->post('/jornalero', [$this,'createJornalero']);
+    $app->post('/jornalero', [$this,'createJornalero']);
+    $app->post('/jornalero', [$this,'createJornalero']);
+    $app->post('/jornalero', [$this,'createJornalero']);
+    $app->post('/jornalero', [$this,'createJornalero']);
+  }
+
+  public function createJornalero(Request $request, Response $response, array $args){
+
+  }
 }
 
 ?>
